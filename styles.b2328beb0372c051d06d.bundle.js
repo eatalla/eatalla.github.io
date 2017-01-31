d95831850ee90e03e89e11343d07025d40f04d5a
Ehab Atalla (Migration) <EHABA@amdocs.com>
2017-01-31 12:12:23 +0200

    more dev

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([3,5],{380:function(n,o){},917:function(n,o,c){n.exports=c(380)}},[917]);
+webpackJsonp([3,5],{380:function(n,o){},915:function(n,o,c){n.exports=c(380)}},[915]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
